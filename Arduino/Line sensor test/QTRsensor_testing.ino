d3d925d9739377d28605537e94d6ca20664828d5
nhathuy1812 <61997203+NhatHuy1812@users.noreply.github.com>
2022-02-22 07:37:07 +0700

    Update QTRsensor_testing.ino

--- Arduino/Line sensor test/QTRsensor_testing.ino
+++ Arduino/Line sensor test/QTRsensor_testing.ino
@@ -1,22 +1,9 @@
-// Thư viện đèn
 #include <QTRSensors.h>
 
 QTRSensors qtr;
-QTRSensors qtr2;
 
-uint16_t position=0;
-uint16_t position2=0;
-float previous_error = 0, previous_I = 0;
-const uint8_t SensorCount = 5;
+const uint8_t SensorCount = 10;
 uint16_t sensorValues[SensorCount];
-uint16_t sensorValues2[SensorCount];
-// Servo steering;
-
-unsigned long previousTime=0;
-unsigned long previousTime2=0;
-
-
-/////////////////////////////////////////////////////////////////////////////////////////
 
 void SensorCalibrate(){
   // configure the sensors
@@ -53,92 +40,26 @@ void SensorCalibrate(){
   delay(1000);
 }
 
-void SensorCalibrate1(){
-  // configure the sensors
-  qtr.setTypeRC();
-  qtr.setSensorPins((const uint8_t[]){25,26,27,32,33}, SensorCount);
-  pinMode(2, OUTPUT);
-  digitalWrite(2, HIGH); // turn on Arduino's LED to indicate we are in calibration mode
-
-  // 2.5 ms RC read timeout (default) * 10 reads per calibrate() call
-  // = ~25 ms per calibrate() call.
-  // Call calibrate() 400 times to make calibration take about 10 seconds.
-  for (uint16_t i = 0; i < 400; i++)
-  {
-    qtr.calibrate();
-  }
-  digitalWrite(2, LOW); // turn off Arduino's LED to indicate we are through with calibration
-
-  // print the calibration minimum values measured when emitters were on
-  for (uint8_t i = 0; i < SensorCount; i++)
-  {
-    Serial.print(qtr.calibrationOn.minimum[i]);
-    Serial.print(' ');
-  }
-  Serial.println();
-
-  // print the calibration maximum values measured when emitters were on
-  for (uint8_t i = 0; i < SensorCount; i++)
-  {
-    Serial.print(qtr.calibrationOn.maximum[i]);
-    Serial.print(' ');
-  }
-  Serial.println();
-  Serial.println();
-  delay(1000);
-}
-
-void SensorCalibrate2(){
-  // configure the sensors
-  qtr2.setTypeRC();
-  qtr2.setSensorPins((const uint8_t[]){16, 17, 18, 19, 21, 25, 26, 27,32,33}, SensorCount);
-
-  delay(500);
-  pinMode(2, OUTPUT);
-  digitalWrite(2, HIGH); // turn on Arduino's LED to indicate we are in calibration mode
-
-  // 2.5 ms RC read timeout (default) * 10 reads per calibrate() call
-  // = ~25 ms per calibrate() call.
-  // Call calibrate() 400 times to make calibration take about 10 seconds.
-  for (uint16_t i = 0; i < 400; i++)
-  {
-    qtr2.calibrate();
-  }
-  digitalWrite(2, LOW); // turn off Arduino's LED to indicate we are through with calibration
-
-  // print the calibration minimum values measured when emitters were on
-  for (uint8_t i = 0; i < SensorCount; i++)
-  {
-    Serial.print(qtr2.calibrationOn.minimum[i]);
-    Serial.print(' ');
-  }
-  Serial.println();
-  
-  // print the calibration maximum values measured when emitters were on
-  for (uint8_t i = 0; i < SensorCount; i++)
-  {
-    Serial.print(qtr2.calibrationOn.maximum[i]);
-    Serial.print(' ');
-  }
-  Serial.println();
-  Serial.println();
-  delay(1000);
-}
-
-//////////////////////////////////////////////////////////////////////////////////////////////
-
-void setup()
-{
+void setup(){
   Serial.begin(115200);
-  // SensorCalibrate1();
-  // SensorCalibrate2();
   SensorCalibrate();
 }
 
 void loop()
 {
-  // Đọc inputs từ Firebase
-  position = qtr.readLineBlack(sensorValues);
-  Serial.println("Sensor: "+String(position));
-  // position2= 4000-qtr2.readLineBlack(sensorValues2);
+  // read calibrated sensor values and obtain a measure of the line position
+  // from 0 to 5000 (for a white line, use readLineWhite() instead)
+  uint16_t position = qtr.readLineBlack(sensorValues);
+
+  // print the sensor values as numbers from 0 to 1000, where 0 means maximum
+  // reflectance and 1000 means minimum reflectance, followed by the line
+  // position
+  for (uint8_t i = 0; i < SensorCount; i++)
+  {
+    Serial.print(sensorValues[i]);
+    Serial.print('\t');
+  }
+  Serial.println(position);
+
+  delay(250);
 }
